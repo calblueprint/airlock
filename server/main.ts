58bd321922699db90ad8783bf7d00a523f4d8b32
Ethan Lee <hello@ethanl.ee>
2020-04-30 01:36:12 -0700

    Add Access-Control-Allow-Methods header

--- server/main.ts
+++ server/main.ts
@@ -197,6 +197,10 @@ class Airlock {
       }
 
       res.header(
+        'Access-Control-Allow-Methods',
+        ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'].join(', '),
+      );
+      res.header(
         'Access-Control-Allow-Headers',
         [
           'Origin',
